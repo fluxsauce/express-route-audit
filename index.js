b66e13dcb4607914dedd1512d624c47adfa67b9a
Jon Peck <jpeck@fluxsauce.com>
2019-02-22 14:45:15 -0800

    Parse router

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const listEndpoints = require('express-list-endpoints')
+
 let pathMethodCounts;
 if (pathMethodCounts === undefined) {
   pathMethodCounts = new Map();
@@ -21,46 +23,43 @@ function generateKey(route, method) {
 }
 
 /**
- * Middleware
+ * Middleware that records the route paths upon a finished request.
  *
  * @param {Object} request - Express request object.
  * @param {Object} response - Express response object.
  * @param {Function} next - Express next middleware function.
+ * @return {*} next
  */
 function middleware(request, response, next) {
   response.on('finish', () => {
     if (request.route && request.route.path) {
       const key = generateKey(request.route.path, request.method);
-      let count = pathMethodCounts.get(key);
-      if (!count) {
-        count = 0;
-      }
+      const count = pathMethodCounts.get(key) || 0;
       pathMethodCounts.set(key, count + 1);
     }
   });
-  next();
+  return next();
 }
 
 function report(app) {
   const routes = [];
-  Object.keys(app._router.stack).forEach((key) => {
-    if (!app._router.stack[key].route) { // eslint-disable-line no-underscore-dangle
-      return;
-    }
-    const routerMiddleware = app._router.stack[key]; // eslint-disable-line no-underscore-dangle
-    console.log(routerMiddleware);
-    const { route: { path } } = routerMiddleware;
-    const { route: { stack: [{ method }] } } = routerMiddleware;
-    let count = pathMethodCounts.get(generateKey(path, method));
-    if (!count) {
-      count = 0;
-    }
-
-    routes.push({
-      path,
-      method,
-      count,
+  listEndpoints(app).forEach((endpoint) => {
+    endpoint.methods.forEach((method) => {
+      const { path } = endpoint;
+      const key = generateKey(path, method);
+      const count = pathMethodCounts.get(key) || 0;
+      routes.push({
+        path,
+        method,
+        count,
+      });
     });
+  });
+  routes.sort((a, b) => {
+    if (a.count > b.count) {
+      return -1;
+    }
+    return (a.count < b.count) ? 1 : 0;
   });
   return routes;
 }
