8abde472b9a9bbd4123bbe865ebc8f104ff3cded
Jon Peck <jpeck@fluxsauce.com>
2019-02-21 16:09:16 -0800

    Building out

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let aggregator;
-if (aggregator === undefined) {
-  aggregator = new Map();
+let pathMethodCounts;
+if (pathMethodCounts === undefined) {
+  pathMethodCounts = new Map();
 }
 
 /**
@@ -20,6 +20,53 @@ function generateKey(route, method) {
   return `${route.toLowerCase()}|${method.toLowerCase()}`;
 }
 
+/**
+ * Middleware
+ *
+ * @param {Object} request - Express request object.
+ * @param {Object} response - Express response object.
+ * @param {Function} next - Express next middleware function.
+ */
+function middleware(request, response, next) {
+  response.on('finish', () => {
+    if (request.route && request.route.path) {
+      const key = generateKey(request.route.path, request.method);
+      let count = pathMethodCounts.get(key);
+      if (!count) {
+        count = 0;
+      }
+      pathMethodCounts.set(key, count + 1);
+    }
+  });
+  next();
+}
+
+function report(app) {
+  const routes = [];
+  Object.keys(app._router.stack).forEach((key) => {
+    if (!app._router.stack[key].route) { // eslint-disable-line no-underscore-dangle
+      return;
+    }
+    const routerMiddleware = app._router.stack[key]; // eslint-disable-line no-underscore-dangle
+    console.log(routerMiddleware);
+    const { route: { path } } = routerMiddleware;
+    const { route: { stack: [{ method }] } } = routerMiddleware;
+    let count = pathMethodCounts.get(generateKey(path, method));
+    if (!count) {
+      count = 0;
+    }
+
+    routes.push({
+      path,
+      method,
+      count,
+    });
+  });
+  return routes;
+}
+
 module.exports = {
   generateKey,
+  middleware,
+  report,
 };
